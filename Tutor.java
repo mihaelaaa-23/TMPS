967a779c207751cb04cec7e74a022df151ca39f6
Mihaela <mihaela.untu@gmail.com>
2025-10-08 14:48:54 +0300

    Add Tutor class to manage tutor data following the Single Responsibility Principle

---
+++ Tutor.java
@@ -0,0 +1,17 @@
+// Tutor class only stores tutor data (Single Responsibility Principle)
+public class Tutor {
+    private final String name;
+
+    public Tutor(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public String toString(){
+        return "Tutor{name='" + name + "'}";
+    }
+}
