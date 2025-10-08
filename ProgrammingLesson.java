5e1ba81f86f43248214f243e40ba5b02e29ea8e6
Mihaela <mihaela.untu@gmail.com>
2025-10-08 14:56:17 +0300

    Add ProgrammingLesson and SMSNotifier classes to implement lesson teaching and SMS notification functionality to show O and D

---
+++ ProgrammingLesson.java
@@ -0,0 +1,6 @@
+public class ProgrammingLesson implements Lesson {
+    @Override
+    public void teach() {
+        System.out.println("Teaching Programming lesson...");
+    }
+}
---
+++ SMSNotifier.java
@@ -0,0 +1,6 @@
+public class SMSNotifier implements Notifier {
+    @Override
+    public void send(String message) {
+        System.out.println("Sending SMS notification: " + message);
+    }
+}
