afa87510a84737ea32d9249a536e167256e016a6
Mihaela <mihaela.untu@gmail.com>
2025-10-08 14:51:54 +0300

    Add BookingManager and Notifier interfaces to implement lesson booking functionality to adhere Dependency Inversion

---
+++ BookingManager.java
@@ -0,0 +1,13 @@
+public class BookingManager {
+    private final Notifier notifier;
+
+    public BookingManager(Notifier notifier) {
+        this.notifier = notifier;
+    }
+
+    public void bookLesson(Tutor tutor, Lesson lesson) {
+        System.out.println("Booking lesson with " + tutor.getName());
+        lesson.teach();
+        notifier.send("Lesson booked with " + tutor.getName());
+    }
+}
---
+++ Notifier.java
@@ -0,0 +1,6 @@
+// Notifier and BookingManager interface demonstrate Dependency Inversion Principle - 
+// High-level modules should not depend on low-level modules. Both should depend on abstractions.
+// Abstractions should not depend on details. Details should depend on abstractions.
+public interface Notifier {
+    void send(String message);
+}
