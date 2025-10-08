d7b69e2439a498d0ba649a2ee5f118ac9c0f2735
Mihaela <mihaela.untu@gmail.com>
2025-10-08 14:49:08 +0300

    Add Lesson interface to define the contract for lesson types adhering to the Open/Closed Principle

---
+++ Lesson.java
@@ -0,0 +1,5 @@
+// Lesson interface defines the contract for different types of lessons - Open/Closed Principle - 
+// classes should be open for extension but closed for modification
+public interface Lesson {
+    void teach();
+}
