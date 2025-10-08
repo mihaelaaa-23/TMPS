0e892858a4e470a0c7cb879af6ff28a6751c2874
Mihaela <mihaela.untu@gmail.com>
2025-10-08 14:53:48 +0300

    Add EnglishLesson and MathLesson classes to implement lesson teaching functionality

---
+++ EnglishLesson.java
@@ -0,0 +1,6 @@
+public class EnglishLesson implements Lesson {
+    @Override
+    public void teach() {
+        System.out.println("Teaching English lesson...");
+    }
+}
---
+++ MathLesson.java
@@ -0,0 +1,6 @@
+public class MathLesson implements Lesson {
+    @Override
+    public void teach() {
+        System.out.println("Teaching Math lesson...");
+    }
+}
