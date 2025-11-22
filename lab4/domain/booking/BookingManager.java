76af143ba4e550b968a80a883a8d33cabdcb5b31
Mihaela <mihaela.untu@gmail.com>
2025-11-22 16:24:36 +0200

    Integrate Observer pattern into BookingManager for enhanced booking notifications

--- lab4/domain/booking/BookingManager.java
+++ lab4/domain/booking/BookingManager.java
@@ -1,9 +1,12 @@
 package lab4.domain.booking;
 import lab4.domain.models.Tutor;
 import lab4.domain.models.Lesson;
-// Singleton class to manage bookings
-public class BookingManager {
+import lab4.domain.observer.BookingSubject;
+
+// Singleton class to manage bookings - now with Observer pattern integration
+public class BookingManager extends BookingSubject {
     private static volatile BookingManager instance;
+    
     private BookingManager() {}
 
     public static BookingManager getInstance() {
@@ -19,6 +22,26 @@ public class BookingManager {
 
     public void bookLesson(Tutor tutor, Lesson lesson) {
         System.out.println("Booking " + lesson.getClass().getSimpleName() + " with " + tutor.toString());
+        
+        // Notify observers about the booking
+        notifyObservers("BOOKING_CONFIRMED", 
+            lesson.getClass().getSimpleName() + " booked with " + tutor.getName());
     }
-
+    
+    public void startLesson(Tutor tutor, Lesson lesson) {
+        notifyObservers("LESSON_STARTING",
+            "Lesson with " + tutor.getName() + " is starting now!");
+    }
+    
+    public void completeLesson(Tutor tutor, Lesson lesson) {
+        notifyObservers("LESSON_COMPLETED",
+            "Lesson with " + tutor.getName() + " completed successfully!");
+    }
+    
+    public void cancelBooking(Tutor tutor, Lesson lesson) {
+        System.out.println("Cancelling booking: " + lesson.getClass().getSimpleName() + " with " + tutor.getName());
+        
+        notifyObservers("BOOKING_CANCELLED",
+            lesson.getClass().getSimpleName() + " with " + tutor.getName() + " has been cancelled.");
+    }
 }
